948989767ba4b6d61d0c1084301c682de4cb5a47
Sohrab Behdani <behdanisohrab@gmail.com>
2024-03-24 15:26:23 +0330

    removed uefi grub boot due some issues

--- iso/profiledef.sh
+++ iso/profiledef.sh
@@ -3,14 +3,14 @@
 
 iso_name="Parchlinux Gnome"
 iso_label="PARCHLINUX_Gnome_$(date +%Y%m)"
-iso_publisher="Parch Linux <https://parchlinux.ir>"
+iso_publisher="Parch Linux <https://parchlinux.com>"
 iso_application="Parch Linux Live/Rescue CD"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
-	   'uefi-ia32.grub.esp' 'uefi-x64.systemd-boot.esp'
-	   'uefi-ia32.grub.eltorito' 'uefi-x64.systemd-boot.eltorito')
+	   'uefi-x64.systemd-boot.esp'
+	   'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
